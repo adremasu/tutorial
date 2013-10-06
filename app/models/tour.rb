9d6264ebe4fde01ca6d8dfe7298078b0571b4621
adremasu <adremasu@gmail.com>
2013-10-06 23:01:42 +0200

    updated tranks and tours

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,3 +1,7 @@
 class Tour < ActiveRecord::Base
+  has_many :traks
+  before_destroy :ensure_not_referenced_by_any_track
+
   attr_accessible :title, :desc_eng, :desc_fra, :desc_ger, :desc_ita
+  validates: :title, presence: true
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,5 @@
 class Track < ActiveRecord::Base
   attr_accessible :points, :title, :tour_id
+  belongs_to :tour
+
 end
